f25856c888dca1d1579d7bf90a0dd71d9ef81b98
Your Name <you@example.com>
2025-04-15 09:03:21 -0700

    More content

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: false
 
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
+    }
 
 })
